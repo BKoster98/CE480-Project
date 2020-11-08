54371f1c4d5c428ca47394eba0c7f22c60b50e62
Allison Hurley <allison@hurleyhome.com>
2020-11-08 14:45:57 -0500

    Added Cmake files, initial client and server communications and messages

--- phase2/phase2client.c
+++ phase2/phase2client.c
@@ -6,112 +6,35 @@
 //  Citation : Based off of sample code found at https://www.binarytides.com/winsock-socket-programming-tutorial/
 
 #include <stdio.h>
-#include <winsock2.h>
-#include <WS2tcpip.h>
-#include <windows.h>
+#include <string.h>
 
-// This pragma ONLY works in Visual Studio. You will need to manually link this library when using something else.
-#pragma comment(lib,"ws2_32.lib") //Winsock Library - don't touch this.
+#include "portable_socket.h"
 
-CHAR ipaddr[INET_ADDRSTRLEN] = "127.0.0.1"; // CE-480 This is the hard-coded address for the *server*, NOT this client. You will need to change this to talk to another computer
-//NOTE - If your project is using Unicode instead of ASCII, you will probably need to change CHAR to WCHAR
-
-
-// create some constants for error codes if the program dies... helps figure out where things went wrong.
-#define ERROR_WINSOCK_INIT_FAILURE				1;
-#define ERROR_WINSOCK_SOCKET_CREATE_FAILURE		2;
-#define ERROR_WINSOCK_SOCKET_CONNECT_FAILURE	3;
-#define ERROR_WINSOCK_SOCKET_SEND_FAILURE       4;
-#define ERROR_WINSOCK_SOCKET_BIND_FAILURE       5;
-
-struct sockaddr_in addrServer;    // IN_ADDR holds an IPv4 address
-
-#define MSGSENDLENGTH 100		   
-CHAR message[MSGSENDLENGTH];
-//NOTE - If your project is using Unicode instead of ASCII, you will probably need to change CHAR to WCHAR
-
-CHAR * ipv4addr;
-
-#define MSGRECVLENGTH 100		   
-CHAR msgRecv[MSGRECVLENGTH];       // array to hold received messages
-//NOTE - If your project is using Unicode instead of ASCII, you will probably need to change CHAR to WCHAR
+CHAR ipaddr[INET_ADDRSTRLEN] = "127.0.0.1";
 
 int main(int argc, char *argv[])
 {
-	WSADATA wsa;
-	SOCKET s;
-// no need to change anything from here...
-	// Before using Winsock calls on Windows, the Winsock library needs to be initialized...
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-	{
-		printf("Failed. Winsock error Code : %d", WSAGetLastError());
-		return ERROR_WINSOCK_INIT_FAILURE;
-	}
+    Socket server, client;
+    int port = 60481;
+    Message msg;
+    char* line = NULL;
+    size_t bytes, size = 0;
 
-	/* The socket() call takes three arguments. The first is the network protocol "Address Family", hence the AF_prefix.
-	The two most common are AF_INET for IPv4 and AF_INET6 for IPv6. The next asks for the port type, which is usually a
-	TCP port with SOCK_STREAM, or a UDP port with SOCK_DGRAM. The third parameter is the specific protocol, such as ICMP,
-	IGMP, or for the purposes of the program, TCP, which uses the constant IPPROTO_TCP. */
-	
-	if ((s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
-	{
-		printf("Could not create socket : %d", WSAGetLastError());
-		return ERROR_WINSOCK_SOCKET_CREATE_FAILURE;
-	}
-	// The socket now exists...but it isn't configured with an IP address or port number yet.
+    initialize_sockets();
+    connect_to_server(&server, ipaddr, port);
 
-	ipv4addr = ipaddr;  
-	InetPton(AF_INET, ipv4addr , &addrServer.sin_addr.s_addr);  // converts the IP address as a text string in ipv4addr to a number
-	addrServer.sin_family = AF_INET;							// Must agree with the socket Address Family type
-// ... to here
-	addrServer.sin_port = htons(60481);							// htons() converts the host endianness to network endianness
-																// This should always be used when transmitting integers
-																// ntohs() converts the opposite way for receiving integers.
-	//CE-480 The socket port number above will need to be changed to match the port that the server is listening on. 
-	// You do NOT specify anything about the client's port number. The OS will pick one for you.
-	{
-		InetNtop(AF_INET, &(addrServer.sin_addr), (PSTR)ipv4addr, INET_ADDRSTRLEN);   // converts the IP address as a 32-bit number to a printable character array
-		printf("Attempting to connect to %s on port %d...\n", ipv4addr, ntohs(addrServer.sin_port));
-	}
-	//Connect to remote server
-	if (connect(s, (struct sockaddr *) &addrServer, sizeof(addrServer)) < 0)   // negative return values indicate an error. For this demo/project, we don't need to troubleshoot, so just "scream and die" 
-	{
-		printf("Could not connect socket");
-		return ERROR_WINSOCK_SOCKET_CONNECT_FAILURE;
-	}
-	
-	// At this point, the connection is established. The rest of the code is customized depending on the order of messages. It needs to be written carefully
-	// to correspond to what the server expects. For example, the server starts be sending the number of the connection, so the server starts with a send(), and the client starts with a recv().
-	
-	int bytesRecv, mynumber;
-	bytesRecv = recv(s, msgRecv, MSGRECVLENGTH, 0);  // blocks here until something is received on the socket.
-	
-	if (bytesRecv > 0) {
-		printf("%s\n", msgRecv);
-	}
+    while (read_msg(&server, &msg) > 0) {
+        printf("%s", msg.buffer);
 
-	sscanf_s(msgRecv, "You are connection %d", &mynumber); // Shows how to use the scanf for a buffer to pick out a number
-	// ...recalling that for a scan, you must supply the ADDRESS of where to store the value
-	printf("I was just told that I'm number %d\n", mynumber);
+        if ((bytes = getline(&line, &size, stdin)) > 0) {
+            line[bytes - 1] = 0;
+            send_string(&server, line);
+        } else {
+            close_socket(&server);
+            break;
+        }
+    }
 
-	printf("Going to sleep for 5 seconds to show that the server will block on a recv() call...\n");
-	Sleep(5000);
-
-	char message[80];
-	printf("Enter your message:");
-	gets(message); // assumes that the user types 79 characters or less.
-	
-	if (send(s, message, (int)strlen(message), 0) < 0) // negative return values indicate an error. For this demo/project, we don't need to troubleshoot, so just "scream and die" 
-	{
-		printf("Error sending message");
-		return ERROR_WINSOCK_SOCKET_SEND_FAILURE;
-	}
-
-	// In more complex interactions, the client may loop. This simple demo just exits after one message.
-
-	printf("Terminating...");
-	Sleep(5000);
-	closesocket(s);
-	WSACleanup();
-	return 0;
+    cleanup_sockets();
+    return 0;
 }
--- phase2/phase2server.c
+++ phase2/phase2server.c
@@ -1,165 +1,130 @@
-// Author : Dr. Foster
-// Purpose : demonstration of winsock API using a simple server
-//
-//
+// Author : Allison Hurley and ???
+// Purpose : Complete requirements for Phase 2
 //
 //  Citation : Based off of sample code found at https://www.binarytides.com/winsock-socket-programming-tutorial/
 //  Reference : http://beej.us/guide/bgnet/ has a decent explanation of the concepts, just be warned that the sample
 //  code is intended for Linux and needs occasional tweaks to run on Windows.
 
 #include <stdio.h>
-#include <winsock2.h>
-#include <WS2tcpip.h>
-#include <windows.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-// This statement ONLY works in Visual studio. In another IDE, you will manually need to link this library.
-#pragma comment(lib,"ws2_32.lib") //Winsock Library - don't touch this line. 
+#include "portable_socket.h"
 
-// create some constants for error codes if the program dies...
-#define ERROR_WINSOCK_INIT_FAILURE				1;
-#define ERROR_WINSOCK_SOCKET_CREATE_FAILURE		2;
-#define ERROR_WINSOCK_SOCKET_CONNECT_FAILURE	3;
-#define ERROR_WINSOCK_SOCKET_SEND_FAILURE       4;
-#define ERROR_WINSOCK_SOCKET_BIND_FAILURE       5;
+enum {
+    EXIT_FLAG = 1,
+    CLOSE_FLAG,
+    CLIENT_DISCONNECT
+};
 
-struct sockaddr_in socketServer, socketClient;   // sockets are used to access the network
-char * msgSend;
-#define MSGRECVLENGTH 100
-char msgRecv[MSGRECVLENGTH];
-CHAR ipv4addr[INET_ADDRSTRLEN] = "127.0.0.1";  // hard-coded just to test functionality
-//NOTE - If your project is using Unicode instead of ASCII, you will probably need to change CHAR to WCHAR in the line above
+int ascii_hex_to_int(char ch) {
+    ch = tolower(ch);
+    if (ch >= '0' && ch <= '9') return ch - '0';
+    if (ch >= 'a' && ch <= 'f') return ch - 'a' + 10;
 
-#define MSGSENDLENGTH 100		   
-CHAR message[MSGSENDLENGTH];
+    // should never happen...
+    return 0;
+}
+
+char hex_to_ascii(int v) {
+    if (v >= 0 && v <= 9) return '0' + v;
+    if (v >= 10 && v <= 15) return 'a' + (v - 10);
+
+    // should never happen...
+    return ' ';
+}
+
+const char* convert_mac(const char* mac) {
+    // not a thread safe approach...
+    static char result[256];
+
+    if (strlen(mac) != 17) {
+        return 0;
+    }
+
+    result[ 0] = tolower(mac[0]);
+    result[ 1] = hex_to_ascii(ascii_hex_to_int(mac[1]) | 0x2);
+    result[ 2] = tolower(mac[3]);
+    result[ 3] = tolower(mac[4]);
+    result[ 4] = ':';
+
+    result[ 5] = tolower(mac[6]);
+    result[ 6] = tolower(mac[7]);
+    result[ 7] = 'f';
+    result[ 8] = 'f';
+    result[ 9] = ':';
+
+    result[10] = 'f';
+    result[11] = 'e';
+    result[12] = tolower(mac[9]);
+    result[13] = tolower(mac[10]);
+    result[14] = ':';
+
+    result[15] = tolower(mac[12]);
+    result[16] = tolower(mac[13]);
+    result[17] = tolower(mac[15]);
+    result[18] = tolower(mac[16]);
+    result[19] = 0;
+
+    return result;
+}
+
+int process_client(Socket* client) {
+    char resp[255] = "";
+    Message msg;
+    int rc = 0;
+
+    while(rc == 0) {
+        msg.length = sprintf(msg.buffer, "%sEnter a MAC Address, Q quit, or T to terminate server: ", resp);
+        send_msg(client, &msg);
+        read_msg(client, &msg);
+        if (msg.length == 1) {
+            if (*msg.buffer == 'T') {
+                printf("Shutting Down\n");
+                rc = EXIT_FLAG;
+            }
+            if (*msg.buffer == 'Q') {
+                printf("Terminate Client\n");
+                rc = CLOSE_FLAG;
+            }
+        } else if (msg.length > 0) {
+
+            // is this a MAC address? do something useful...
+            const char* ipv6 = convert_mac(msg.buffer);
+            if (ipv6) {
+                sprintf(resp, "The EUI-64 number is %s.\n", ipv6);
+            } else {
+                resp[0] = 0;
+                printf("Unable to process >%s<\n", msg.buffer);
+            }
+
+        } else {
+            printf("Client disconnect\n");
+            rc = CLIENT_DISCONNECT;
+        }
+    }
+
+    close_socket(client);
+    return rc;
+}
 
 int main(int argc, char *argv[])
 {
-	// In traditional C, all variables must be declaring in a code block before any executable code. In C++, they can (and should) be declared right before their first use.
-	WSADATA wsa;
-	SOCKET s, s_new;
-	int c;
-	int connections = 0;
+    // todo use argc/argv to get optional port command....
+    Socket server, client;
+    int port = 60481;
 
-// CE-480 No need to change anything from here....
-	// Before using Winsock calls on Windows, the Winsock library needs to be initialized...     
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-	{
-		printf("Failed. Winsock error Code : %d", WSAGetLastError());
-		return ERROR_WINSOCK_INIT_FAILURE;
-	}
+    initialize_sockets();
+    initialize_server(&server, port);
 
-	/* The socket() call takes three arguments. The first is the network protocol "Address Family", hence the AF_prefix.
-	The two most common are AF_INET for IPv4 and AF_INET6 for IPv6. The next asks for the port type, which is usually a
-	TCP port with SOCK_STREAM, or a UDP port with SOCK_DGRAM. The third parameter is the specific protocol, such as ICMP,
-	IGMP, or for the purposes of this chat program, TCP, which uses the constant IPPROTO_TCP. */
+    while (wait_for_client(&server, &client) == 0) {
+        printf("Connection established with %s\n", ipv4addr(&client));
+        if (process_client(&client) == EXIT_FLAG) break;
+    }
 
-	if ((s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
-	{
-		printf("Could not create socket : %d", WSAGetLastError());
-		return ERROR_WINSOCK_SOCKET_CREATE_FAILURE;
-	}
-//  ...to here
+    cleanup_sockets();
 
-	// The socket now exists...but it isn't configured with an IP address or port number yet.
-	
-	{   // This block sets up the IP address that the server will listen on (in case is has multiple IP addresses) and the port
-		//InetPton(AF_INET, ipv4addr, &socketServer.sin_addr.s_addr); // The variable ip4addr is a character array that has an IPv4 address in it. The code defines it above to
-																	// to 127.0.0.1, so it will only talk to itself. The  InetPton function converts this text string to a 32-bti number and stores
-																	// it in the socket socketServer.
-		//or allow the system to select one
-		socketServer.sin_addr.s_addr = INADDR_ANY;					// NOTE!!! when you use INADDR_ANY, the host will print 0.0.0.0 as the IP Address, but it is listening on all IP's.
-		socketServer.sin_family = AF_INET;							// Must agree with the socket Address Family type
-
-		//CE-480 - Change the port number that the server is using by altering the following line.
-		socketServer.sin_port = htons(60481);						// htons() converts the host endianness to network endianness
-																	// This should always be used when transmitting integers
-																	// ntohs() converts the opposite way for receiving integers.
-	}
-
-	if (bind(s, (struct sockaddr *) &socketServer, sizeof(socketServer)) == SOCKET_ERROR)
-	{
-		printf("Could not bind socket");
-		return ERROR_WINSOCK_SOCKET_BIND_FAILURE;
-	}
-	
-		// converts between the IP address as a number to a printable string in ipv4addr
-
-	{ // This block uses some common functions to print some feedback. Nothing affecting the socket happens here.
-		InetNtop(AF_INET, &(socketServer.sin_addr), (PSTR)ipv4addr, INET_ADDRSTRLEN);      // The InetNtop converts the 32-bit IP address to a printable string in dotted decimal notation.
-		printf("Listening on IP address %s port %d...\n", ipv4addr, ntohs(socketServer.sin_port));  // Note that teh ntohs() function is always used to convert the socket number 
-																									// in network-endianness to the computer's endianness before printing it.
-	}
-	listen(s, 5);	// tells socket to start listening for clients, 5 is the arbitrarily chosen number of backlogged connections allowed
-					// This only needs to be done once on the server's original socket.
-	
-	while (connections < 5) {    // Normally a server runs "forever", but this demo will exit after 5 connections.
-		connections++;
-		// the server will keep accepting new connectiions the "listening" socket. 
-		c = sizeof(struct sockaddr_in);
-		s_new = accept(s, (struct sockaddr *)&socketClient, &c); // This call will block until a client attempts to connect. At that point,
-																 // a new socket is created (stored in s_new) that is a complete socket between this server
-																 // and the client. The orginal socket s still existed to accept new calls. Information about the connection, 
-																 // such as the client's IP and port number, is stored to socketClient. 
-		if (s_new == INVALID_SOCKET)
-		{
-			printf("Accept failed with error code : %d", WSAGetLastError());
-			continue; // Something went wrong, so the code will loop back to the accept without doing anything.
-		}
-
-		{  // Again, ugly looking function to convert the client's IP address as a 32-bit number to printable text
-			InetNtop(AF_INET, &(socketClient.sin_addr), (PSTR)ipv4addr, INET_ADDRSTRLEN);    // Again, ugly looking function to convert the client's IP address as a 32-bit number to printable text 
-			printf("Accepted a connection from IP address %s port %d...\n", ipv4addr, ntohs(socketClient.sin_port));
-		}
-		// At this point, the server has two sockets open. Since it probably needs to send and receive data with s_new to talk to the client, it can't accept another connection,
-		// since both recv() and accept() block. This is the spot in the code to create a new thread and pass it s_new, and the thread code can send and recv. The main program
-		// can then loop back and try to accept() a new call.
-		//
-		// Since the main focus isn't multithreading calls, create a fixed number of threads (at least 5), and sequentially create them as new calls come in.
-		// Once the last thread has been created, this main program should print a message that it is out of threads, and the user should wait to close
-		// the main program until the clients are done.
-		//
-		// This section should be moved to the thread function. Note that the sequence of send() and recv() functions will be dictated by the 
-		// sequence of messages the program needs to exchange. In this example, once the connection is established, the client sends first.
-		
-		sprintf_s(message, MSGSENDLENGTH, "You are connection %d.", connections); // the sprintf is like printf, but the string goes into
-		// a variable (the first arugument message), and the _s means this is the safe version that limits the copied characters to the 
-		// value in the second argument MSGSENDLENGTH)
-		send(s_new, message, (int)strlen(message), 0);
-		if (send(s_new, message, (int)strlen(message), 0) < 0) // negative return values indicate an error. For this demo/project, we don't need to troubleshoot, so just "scream and die" 
-		{
-			printf("Error sending message");
-			return ERROR_WINSOCK_SOCKET_SEND_FAILURE;
-		}
-
-		int bytesRecv;
-		printf("The server will block here until a message is received...\n");
-		bytesRecv = recv(s_new, msgRecv, MSGRECVLENGTH-1, 0);  // blocks here until something is received on the socket.
-		msgRecv[bytesRecv] = '\0'; // Null terminate the text 
-		// The return value is the count of the number of bytes. The characters are stored in the array msgRecv, and it will read at most MSGRECVLENGTH characters
-
-		// The sample chatClient has a 5 second pause after connecting to show that this program is waiting on recv().
-
-		if ((bytesRecv == 1) && (msgRecv[0] == 'Q'))
-		{
-			printf("Closing up the sockets and exiting gracefully...\n");
-			closesocket(s_new);
-			Sleep(5000);
-			closesocket(s);
-			WSACleanup();
-			exit(0);
-		}
-		else if (bytesRecv > 0)
-		{
-			printf("%s\n", msgRecv);  
-		}
-		closesocket(s_new); // This versions receives only one message per socket, closes it, and waits to accept a new one.
-	}   
-
-	printf("Out of \'threads\".... closing down.\n");
-	Sleep(5000);
-	closesocket(s);
-	WSACleanup();
-	exit(0);
-
-
+    return 0;
 }
+
