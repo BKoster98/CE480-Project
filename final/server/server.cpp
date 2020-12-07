b33e2772210037719a5f358adb56a318e602c54d
Ben Kocik <bkocik@heestand.tech>
2020-12-06 22:37:47 -0500

    Created initial server and client files for final project

---
+++ final/server/server.cpp
@@ -0,0 +1,15 @@
+//Authors: Allison Hurley & Ben Kocik 
+//Purpose: Final server dice roller Cpp program
+
+#include <iostream>    
+#include <thread> 
+#include <mutex>
+// Include our portable socket since it is written in C
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include "../portable_socket.h"
+#ifdef __cplusplus
+}
+#endif
